3f39421dab20c29d1310d0bc348ef44c2a2e0914
Dan Kim <itsdanya@gmail.com>
2018-12-07 21:13:51 +0300

    Implement binding

--- lib/alfabank/client.rb
+++ lib/alfabank/client.rb
@@ -10,11 +10,22 @@ module Alfabank
       Alfabank::Request.new('register.do', params).perform
     end
 
+    # https://web.rbsuat.com/ab/swagger/swagger.html#/status/getOrderStatusExtendedUsingPOST_1
     def self.status(order_id: nil, order_number: nil)
       raise ArgumentError if order_id.nil? && order_number.nil?
 
       params = { orderId: order_id, orderNumber: order_number }.compact
       Alfabank::Request.new('getOrderStatusExtended.do', params).perform
+    end
+
+    # https://pay.alfabank.ru/ecommerce/instructions/merchantManual/pages/index/rest.html#zapros_provedenija_platezha_po_svjazke_rest_
+    def self.payment_order_binding(md_order:, binding_id:, ip:, options: {})
+      params = options.merge(
+        mdOrder: md_order,
+        binding_id: bindingId,
+        ip: ip
+      )
+      Alfabank::Request.new('paymentOrderBinding.do', params).perform
     end
   end
 end
