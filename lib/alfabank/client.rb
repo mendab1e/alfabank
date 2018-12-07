a126e26695ef4cb7a217d3a7f0b458c7eb53cb94
Dan Kim <itsdanya@gmail.com>
2018-12-07 20:40:35 +0300

    Fix params in Client

--- lib/alfabank/client.rb
+++ lib/alfabank/client.rb
@@ -1,11 +1,11 @@
 module Alfabank
   class Client
     # https://web.rbsuat.com/ab/swagger/swagger.html#/register/registerOrderUsingPOST_7
-    def register(order_number:, amount:, return_url:, options: {})
+    def self.register(order_number:, amount:, return_url:, options: {})
       params = options.merge(
-        order_number: order_number,
+        orderNumber: order_number,
         amount: amount,
-        return_url: return_url
+        returnUrl: return_url
       )
       Alfabank::Request.new('register.do', params).perform
     end
